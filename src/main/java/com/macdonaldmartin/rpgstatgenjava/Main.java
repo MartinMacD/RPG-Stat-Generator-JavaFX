06e67650925f4958c31e148844c2ce4a627a4c53
Martin <149177224+MartinMacD@users.noreply.github.com>
2026-09-18 19:55:51 +0100

    Created GUI, need to add logic

--- src/main/java/com/macdonaldmartin/rpgstatgenjava/Main.java
+++ src/main/java/com/macdonaldmartin/rpgstatgenjava/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 330, 570);
+        stage.setTitle("RPG Stats Generator");
         stage.setScene(scene);
         stage.show();
     }
